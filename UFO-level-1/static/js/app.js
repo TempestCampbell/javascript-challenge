3a76ea551100d7415b62d52dab831498d02c414a
Tempest Campbell <TempestCampbell@hotmail.com>
2021-02-21 18:52:39 -0700

    js table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,27 @@
-// from data.js
-var tableData = data;
+var tableInfo = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+function createTable(data) {
+  tbody.html("");
+  data.forEach((dataRow) => {
+    var row = tbody.append("tr");
+
+    Object.values(dataRow).forEach((value) => {
+      let cell = row.append("td");
+      cell.text(value);
+    });
+  });
+}
+
+function handleClick() {
+  var date = d3.select("#datetime").property("value");
+  let filterInfo = tableInfo;
+
+  if (date) {
+    filterInfo = filterInfo.filter((row) => row.datetime === date);
+  }
+  createTable(filterInfo);
+}
+
+d3.selectAll("#filter-btn").on("click", handleClick);
+createTable(tableInfo);
